1deb733b5de8997fbcbc692a830c1593839ee3fa
wheeinmmm <36217386+wheeinmmm@users.noreply.github.com>
2018-02-07 11:01:41 +0700

    Add files via upload

---
+++ mybot.php
@@ -0,0 +1,53 @@
+﻿<?php
+$access_token = 'tcPymzYjnFi2ZteKaJamGyqx19ZyEg/RvO45t+GiX16CVUTzMvAJxHehF3GmKCjwe+Ti1LBX07/w6XmrmXeApgqOVimj4siJ4RrDjZm63OwVCDbYDYs+QpNpTTFMN523N6yZiWJpsuFQkNwg80etfgdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			if($text == 'hello'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'สวัสดีครัชช'
+				];
+			}
+			else if($text == 'test'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'ทดสอบๆ'
+				];
+			}
+		
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+?>
